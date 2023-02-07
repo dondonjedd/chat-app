005269cb898e10486e2aab8013e47615e6ca638e
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2023-02-07 15:22:07 +0800

    create authentication form

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -8,6 +8,24 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  bool _loginMode = true;
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,33 +35,75 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
+                key: _formKey,
                 child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(labelText: 'Email Address'),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Username'),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll(
-                            Theme.of(context).colorScheme.primary)),
-                    child: const Text('Login')),
-                TextButton(
-                    onPressed: () {}, child: const Text('Create New Account')),
-              ],
-            )),
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      key: const ValueKey('email'),
+                      validator: (value) {
+                        if (value!.isEmpty || !value.contains('@')) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration:
+                          const InputDecoration(labelText: 'Email Address'),
+                      onSaved: (newValue) {
+                        _userEmail = newValue!;
+                      },
+                    ),
+                    if (!_loginMode)
+                      TextFormField(
+                        key: const ValueKey('username'),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 4) {
+                            return 'Username needs to be at least 4 characters long';
+                          }
+                          return null;
+                        },
+                        decoration:
+                            const InputDecoration(labelText: 'Username'),
+                        onSaved: (newValue) {
+                          _userName = newValue!;
+                        },
+                      ),
+                    TextFormField(
+                      key: const ValueKey('password'),
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 7) {
+                          return 'Password needs to be at least 7 characters long';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                      onSaved: (newValue) {
+                        _userPassword = newValue!;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    ElevatedButton(
+                        onPressed: _trySubmit,
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll(
+                                Theme.of(context).colorScheme.primary)),
+                        child: Text(_loginMode ? 'Login' : 'Sign up')),
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _loginMode = !_loginMode;
+                          });
+                          _formKey.currentState!.reset();
+                        },
+                        child: Text(_loginMode
+                            ? 'Create new account'
+                            : 'Log in instead')),
+                  ],
+                )),
           ),
         ),
       ),
