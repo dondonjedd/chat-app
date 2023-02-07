5c91a817fd0fd0a4089eef825642de5596ebd434
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2023-02-07 22:04:28 +0800

    create image input UI when signing up

---
+++ lib/widgets/image_picker.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() => ImageInputState();
+}
+
+class ImageInputState extends State<ImageInput> {
+  File? _pickedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    void _pickImage() async {
+      final picker = ImagePicker();
+      final pickedImage = await picker.pickImage(source: ImageSource.camera);
+      if (pickedImage == null) {
+        return;
+      }
+      final pickedImageFile = File(pickedImage.path);
+      setState(() {
+        _pickedImage = pickedImageFile;
+      });
+    }
+
+    return Column(
+      children: [
+        if (_pickedImage != null)
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.transparent,
+            backgroundImage:
+                _pickedImage != null ? FileImage(_pickedImage!) : null,
+          ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: const Icon(Icons.image),
+          label: const Text('Add Image'),
+        ),
+      ],
+    );
+  }
+}
